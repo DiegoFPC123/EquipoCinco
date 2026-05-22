9eed01ec7718724b4ea73af84eade3907231abd4
DiegoFPC123 <dfpayanc2018@gmail.com>
2026-05-21 19:07:07 -0500

    criterio 1,2 y 3 de la historia HU-2

---
+++ app/src/main/java/com/example/pico_botella/ui/home/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.pico_botella.ui.home
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+    // Lógica para los clicks básicos se implementará aquí en el futuro
+}
